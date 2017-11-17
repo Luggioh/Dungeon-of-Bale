d49e3ae99c89de4881fde2b9b488ff251a4befcc
Steve Berthold <steve.berthold@msg.group>
2017-11-17 13:05:26 +0100

    init

---
+++ desktop/src/eu/the_machine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package eu.the_machine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import eu.the_machine.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
