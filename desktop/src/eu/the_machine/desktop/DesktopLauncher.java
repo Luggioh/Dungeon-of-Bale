4c71ace0d404460e2f4f9f94ada057e8dcc7c791
User <User@DESKTOP-BTUJU66.Speedport_W_724V_Typ_A_05011603_05_020>
2017-12-11 21:26:01 +0100

    test

--- desktop/src/eu/the_machine/desktop/DesktopLauncher.java
+++ desktop/src/eu/the_machine/desktop/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new DungeonOfBale(), config);
+		//11.12.17 test
 //ttest
 		//testkommentar
 	}
